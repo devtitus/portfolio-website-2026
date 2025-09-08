69b374630216e5bfc1b9b2bf8251bd659c124fbe
devtitus <m.works.gd@gmail.com>
2025-09-09 02:03:46 +0530

    feat: Integrate Prismic for dynamic skills section
    This commit integrates the Prismic CMS to manage the content for the skills section, replacing the previously hardcoded data.
    
    - Adds a new "My Skills" custom type in Prismic to manage skill names and icons.
    - Implements a `getSkills` query to fetch skill data from the Prismic API.
    - Refactors the `SkillsSection` component to be an async component that dynamically fetches and renders skills.
    - Sets up Prismic client configuration, Slice Machine, and Next.js API routes for preview and revalidation.
    - Installs necessary dependencies, including `@prismicio/client` and `@prismicio/next`.

--- app/pages/home/components/skillsSection.tsx
+++ app/pages/home/components/skillsSection.tsx
@@ -2,8 +2,11 @@ import React from "react";
 import styles from "@/app/styles/home/components/skill.module.css";
 import { SkillSpinningImage } from "@/app/utils/icons";
 import SkillCard from "@/app/components/home/skillCard";
+import { getSkills } from "@/app/queries/getSkills";
 
-const SkillsSection = () => {
+const SkillsSection = async () => {
+  const skills = await getSkills();
+
   return (
     <section className={`${styles.skillSection} ${styles.commonStyleSection}`}>
       <div className={styles.skillSectionWrapper}>
@@ -12,10 +15,13 @@ const SkillsSection = () => {
           <h2 className={styles.skillSectionTitle}>My Skills</h2>
         </div>
         <div className={styles.skillBottomWrapper}>
-          <SkillCard
-            skillName="Skill 1"
-            skillIcon="https://picsum.photos/seed/picsum/200/300"
-          />
+          {skills.map((skill) => (
+            <SkillCard
+              key={skill.id}
+              skillName={skill.label}
+              skillIcon={skill.iconUrl}
+            />
+          ))}
         </div>
       </div>
     </section>
